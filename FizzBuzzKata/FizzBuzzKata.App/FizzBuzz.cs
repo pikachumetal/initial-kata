52a324adac4ca7033cc1c25d091ad6e03793c38e
pikachumetal <pikachu.metal@gmail.com>
2019-05-12 19:21:50 +0200

    FizzBuzz Solution

---
+++ FizzBuzzKata/FizzBuzzKata.App/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzzKata.App
+{
+    public class FizzBuzz
+    {
+        public string GetNumber(int i)
+        {
+            AssertValidityParameters(i);
+            
+            if (HasToShowFizzFuzz(i)) return "FizzFuzz";
+            if (HasToShowFizz(i)) return "Fizz";
+            if (HasToShowFuzz(i)) return "Fuzz";
+
+            return $"{i}";
+        }
+
+        private static bool HasToShowFizz(int i) => i % 3 == 0;
+
+        private static bool HasToShowFuzz(int i) => i % 5 == 0;
+
+        private static bool HasToShowFizzFuzz(int i) => HasToShowFizz(i) && HasToShowFuzz(i);
+
+        private static void AssertValidityParameters(int i)
+        {
+            var isOutOfRange = i < 1 || i > 100;
+            if (isOutOfRange) throw new ArgumentOutOfRangeException(nameof(i));
+        }
+    }
+}
--- FizzBuzzKata/FizzBuzzKata.App/Program.cs
+++ FizzBuzzKata/FizzBuzzKata.App/Program.cs
@@ -1,12 +1,17 @@
 using System;
 
-namespace InitialKata.App
+namespace FizzBuzzKata.App
 {
     internal class Program
     {
         private static void Main()
         {
-            Console.WriteLine("Initial Kata");
+            Console.WriteLine("Fizz Buzz Kata");
+            var fb = new FizzBuzz();
+            for (var i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"[{i}]: {fb.GetNumber(i)}");
+            }
             Console.ReadKey();
         }
     }

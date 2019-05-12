a4816f1eb6609d0e4f96b28525ee8de2dd90837a
pikachumetal <pikachu.metal@gmail.com>
2019-05-12 18:34:10 +0200

    Initial Fizz Buzz

---
+++ FizzBuzzKata/FizzBuzzKata.App/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace InitialKata.App
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Initial Kata");
+            Console.ReadKey();
+        }
+    }
+}
